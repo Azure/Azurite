aa85fef46044e634859501c1b32decf314ef26e0
arafato <oliver.arafat@outlook.com>
2016-12-17 22:12:54 +0100

    added tests

--- test/container.js
+++ test/container.js
@@ -5,11 +5,15 @@ const chai = require('chai'),
 
 chai.use(chaiHttp);
 
-describe('PUT Container', () => {
-    describe('Simple Container', () => {
+describe('Container', () => {
+    const url = 'http://localhost:10000';
+    const path = '/devstoreaccount1';
+    const simpleContainerName = 'simpleContainer'
+
+    describe('PUT Simple Container', () => {
         it('should create a container', (done) => {
-            chai.request('http://localhost:10000')
-                .put('/devstoreaccount1/testcontainer')
+            chai.request(url)
+                .put(`${path}/${simpleContainerName}`)
                 .query({ restype: 'container' })
                 .end((err, res) => {
                     should.not.exist(err);
@@ -18,11 +22,31 @@ describe('PUT Container', () => {
                 });
         });
         it('and a second with the same name that fails', (done) => {
-            chai.request('http://localhost:10000')
-                .put('/devstoreaccount1/testcontainer')
+            chai.request(url)
+                .put(`${path}/${simpleContainerName}`)
                 .query({ restype: 'container' })
                 .end((err, res) => {
                     res.should.have.status(409);
+                    done();
+                });
+        });
+    });
+    describe('DELETE Simple Container', () => {
+        it('successfully deletes the container', (done) => {
+            chai.request(url)
+                .delete(`${path}/${simpleContainerName}`)
+                .query({restype: 'container'})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+        it('deleting a non-existant container fails', (done) => {
+            chai.request(url)
+                .delete(`${path}/DOESNOTEXIST`)
+                .query({restype: 'container'})
+                .end((err, res) => {
+                    res.should.have.status(404);
                     done();
                 });
         });
