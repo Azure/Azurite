545544a7dc420b743219a9fd805c4f348798e2bc
arafato <oliver.arafat@outlook.com>
2016-12-20 17:28:39 +0100

    added unit tests

--- test/blob.js
+++ test/blob.js
@@ -4,47 +4,67 @@ const chai = require('chai'),
     BbPromise = require('bluebird'),
     fs = BbPromise.promisifyAll(require("fs-extra")),
     Azurite = require('./../lib/Azurite'),
+    rp = require('request-promise'),
     path = require('path');
 
 chai.use(chaiHttp);
+
+const containerName = 'testcontainer';
+const url = `http://localhost:10000`;
+const urlPath = '/devstoreaccount1';
 
 describe('Blob HTTP API', () => {
     const azurite = new Azurite();
 
     before(() => {
         const location = path.join(process.env.AZURITE_LOCATION, 'BLOB');
-        return azurite.init({ l: location, silent: 'true', overwrite: 'true' });
+        return azurite.init({ l: location, silent: 'true', overwrite: 'true' })
+            .then(() => {
+                // Make sure there is an existing container 'testcontainer'
+                let options = {
+                    method: 'PUT',
+                    uri: `http://localhost:10000/devstoreaccount1/${containerName}?restype=container`,
+                    body: ''
+                };
+                return rp(options);
+            });
     });
 
     after(() => {
         return azurite.close();
     });
 
-    const url = 'http://localhost:10000';
-    const urlPath = '/devstoreaccount1';
-    const simpleContainerName = 'simpleContainer'
+
 
     describe('PUT Blob', () => {
-        it('should fail to create a block due to missing container', (done) => {
+        it('should fail to create a block due to missing container', () => {
             chai.request(url)
                 .put(`${urlPath}/DOESNOTEXISTS/blob`)
                 .set('x-ms-blob-type', 'BlockBlob')
                 .set('Content-Type', 'application/octet-stream')
                 .send('THIS IS CONTENT')
-                .end((err, res) => {
+                .then((res) => {
                     res.should.have.status(404);
-                    done();
                 });
         });
-        it('should fail to create a block due wrong or unsupported blob type', (done) => {
+        it('should fail to create a block due to wrong or unsupported blob type', () => {
             chai.request(url)
                 .put(`${urlPath}/DOESNOTEXISTS/blob`)
                 .set('x-ms-blob-type', 'NOTSUPPORTED')
                 .set('Content-Type', 'application/octet-stream')
                 .send('THIS IS CONTENT')
-                .end((err, res) => {
+                .then((res) => {
                     res.should.have.status(500);
-                    done();
+                });
+        });
+        it('should create a simple blob without meta headers', () => {
+            chai.request(url)
+                .put(`${urlPath}/${containerName}/blob`)
+                .set('x-ms-blob-type', 'BlockBlob')
+                .set('Content-Type', 'application/octet-stream')
+                .send('THIS IS CONTENT.')
+                .then((res) => {
+                    res.should.have.status(201);
                 });
         });
     });
