1ccabf69913bc75a52b66963df5036acbbeff312
arafato <oliver.arafat@outlook.com>
2016-12-25 20:40:25 +0100

    added Get Container Metadata. Closes #8

--- test/container.js
+++ test/container.js
@@ -85,5 +85,47 @@ describe('Container HTTP API', () => {
                     res.should.have.status(200);
                 });
         });
+        it('should get the correct metadata. (GET)', () => {
+            return chai.request(url)
+                .get(`${urlPath}/${propContainer}`)
+                .query({ restype: 'container', comp: 'metadata' })
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.have.header('x-ms-meta-test1', 'value1');
+                    res.should.have.header('x-ms-meta-test2', 'value2');
+                    res.should.have.header('x-ms-meta-meta1', 'meta1Value');
+                    res.should.have.header('Last-Modified');
+                    res.should.have.header('ETag');
+                });
+        });
+        it('should get the correct metadata. (HEAD)', () => {
+            return chai.request(url)
+                .head(`${urlPath}/${propContainer}`)
+                .query({ restype: 'container', comp: 'metadata' })
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.have.header('x-ms-meta-test1', 'value1');
+                    res.should.have.header('x-ms-meta-test2', 'value2');
+                    res.should.have.header('x-ms-meta-meta1', 'meta1Value');
+                    res.should.have.header('Last-Modified');
+                    res.should.have.header('ETag');
+                });
+        });
+        it('should fail to get metadata of a non-existant container (GET)', () => {
+            return chai.request(url)
+                .get(`${urlPath}/CONTAINER_DOESNOTEXIST`)
+                .query({ restype: 'container', comp: 'metadata' })
+                .catch((e) => {
+                    e.should.have.status(404);
+                });
+        });
+        it('should fail to get metadata of a non-existant container (HEAD)', () => {
+            return chai.request(url)
+                .head(`${urlPath}/CONTAINER_DOESNOTEXIST`)
+                .query({ restype: 'container', comp: 'metadata' })
+                .catch((e) => {
+                    e.should.have.status(404);
+                });
+        });
     });
 });
