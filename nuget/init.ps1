43f8802126e757ab5abac6fe1a5d6e2272c8eb92
arafato <oliver.arafat@outlook.com>
2017-11-06 22:12:27 +0100

    fixed nuget package

--- nuget/init.ps1
+++ nuget/init.ps1
@@ -1,3 +1,4 @@
 param($installPath, $toolsPath, $package, $project)
 
-Start-Process -FilePath (Join-Path $toolsPath 'azurite.exe') -ArgumentList "-l azurite_workspace"
+Start-Process -FilePath (Join-Path $toolsPath 'blob.exe') -ArgumentList "-l azurite_workspace"
+Start-Process -FilePath (Join-Path $toolsPath 'queue.exe') -ArgumentList ""
