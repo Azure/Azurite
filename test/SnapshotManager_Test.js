ed9cd5fa2fb03bba3e1126e3b3acd11524ebf42f
arafato <oliver.arafat@outlook.com>
2017-12-04 12:36:55 +0100

    fixed snapshotmanager time test

--- test/SnapshotManager_Test.js
+++ test/SnapshotManager_Test.js
@@ -6,9 +6,12 @@ const chai = require('chai'),
 
 describe('SnapshotTimeManager', () => {
     it('should return a snapshot date that is at least one second greater than previous snapshot of same container-blob', () => {
-        const timeContext = new Date();
-        const d1 = SnapshotManager.getDate('id1', timeContext);
-        const d2 = SnapshotManager.getDate('id1', timeContext);
-        expect(d2.getSeconds()).to.be.greaterThan(d1.getSeconds());
+        const timeContext = new Date(),
+            d1 = SnapshotManager.getDate('id1', timeContext),
+            d2 = SnapshotManager.getDate('id1', timeContext),
+            d1Seconds = d1.getSeconds(),
+            d2Seconds = d2.getSeconds();
+        if (d2Seconds === 0) d2Seconds = 60;
+        expect(d2Seconds).to.be.greaterThan(d1Seconds);
     });
 });
