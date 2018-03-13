4942d325d7911a9f24a16675adf992698539a473
arafato <arafato@outlook.com>
2018-03-13 21:52:38 +0100

    updated

--- nuget/init.ps1
+++ nuget/init.ps1
@@ -2,3 +2,4 @@ param($installPath, $toolsPath, $package, $project)
 
 Start-Process -FilePath (Join-Path $toolsPath 'blob.exe') -ArgumentList "-l azurite_workspace"
 Start-Process -FilePath (Join-Path $toolsPath 'queue.exe')
+Start-Process -FilePath (Join-Path $toolsPath 'table.exe') -ArgumentList "-l azurite_workspace"
