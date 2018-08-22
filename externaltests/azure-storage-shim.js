9708d461450453d21abb900f5e8f0aaea3af45d6
edwin-huber <edwin.huber@microsoft.com>
2018-08-22 17:05:00 +0200

    minor changes

--- externaltests/azure-storage-shim.js
+++ externaltests/azure-storage-shim.js
@@ -1,6 +1,6 @@
 /**
  * NOTE: the shim requires the following environment variables to be set to work:
- * KNOCK_OFF=true
+ * NOCK_OFF=true
  * AZURE_STORAGE_CONNECTION_STRING=DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;
  * this is automatically handled via env-cmd in the package.json for npm run test
  */
