e6ff362826e6177927e9417bd478d002ad863777
swissarmykirpan <gurpreet@gurpreetsohal.com>
2018-09-08 05:33:40 +0100

    conflicts

--- test/02_01_Blob_Serivce_Tests.js
+++ test/02_01_Blob_Serivce_Tests.js
@@ -1,20 +1,15 @@
 /** @format */
 
-const chai = require("chai"),
-  chaiHttp = require("chai-http"),
-  should = chai.should(),
-  expect = chai.expect,
-  BbPromise = require("bluebird"),
-  fs = BbPromise.promisifyAll(require("fs-extra")),
-  Azurite = require("./../../Azurite/lib/AzuriteBlob"),
-  rp = require("request-promise"),
-  path = require("path"),
-  xml2js = require("xml2js"),
-  azureStorage = require("azure-storage");
+import chai from "chai";
+import chaiHttp from "chai-http";
+import Azurite from "./../../Azurite/lib/AzuriteBlob";
+import azureStorage from "azure-storage";
+/** @format */
+
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-const tableName = "testtable";
 // after testing, we need to clean up the DB files etc that we create.
 // I wanted to shorten the cycles while debugging so create a new path
 // with each pass of the debugger
@@ -28,55 +23,57 @@ const blobService = azureStorage.createBlobService(
 );
 
 describe("Blob Service Tests", () => {
-    const azurite = new Azurite();
-    let serviceProps = {};
-    before(() => {
-        return new Promise((resolve) => {
-            azurite
-                .init({
-                    l: blobServiceTestPath,
-                    silent: "true",
-                    overwrite: "true",
-                })
-                .then(()=>{
-                    const options = {};
-                    blobService.getServiceProperties((options, errOrResult) => {
-                        serviceProps = errOrResult;
-                        resolve();
-                    });
-                });
+  const azurite = new Azurite();
+  let serviceProps = {};
+  beforeAll(() => {
+    return new Promise((resolve) => {
+      azurite
+        .init({
+          l: blobServiceTestPath,
+          silent: "true",
+          overwrite: "true",
+        })
+        .then(() => {
+          blobService.getServiceProperties((options, errOrResult) => {
+            serviceProps = errOrResult;
+            resolve();
+          });
         });
     });
+  });
 
-    // https://docs.microsoft.com/en-us/rest/api/storageservices/get-blob-service-properties
-    // at the time of writing this test Azurite API Version is : "2016-05-31";
-    describe("GET Blob Service Properties", () => {
-        it("should retrieve a valid set of blobService properties", (done) => {
-            expect(serviceProps).to.not.be.equal(undefined);
-            // Logging
-            expect(serviceProps.Logging.Version).to.be.equal("1.0"); 
-            expect(serviceProps.Logging.Delete).to.be.equal(true);
-            expect(serviceProps.Logging.Read).to.be.equal(true);
-            expect(serviceProps.Logging.Write).to.be.equal(true);
-            expect(serviceProps.Logging.RetentionPolicy.Enabled).to.be.equal(false);
-            // Hour Metrics
-            expect(serviceProps.HourMetrics.Version).to.be.equal("1.0"); 
-            expect(serviceProps.HourMetrics.Enabled).to.be.equal(false);
-            expect(serviceProps.HourMetrics.RetentionPolicy.Enabled).to.be.equal(false);
-            // // Minute Metrics
-            expect(serviceProps.MinuteMetrics.Version).to.be.equal("1.0"); 
-            expect(serviceProps.MinuteMetrics.Enabled).to.be.equal(false);
-            expect(serviceProps.MinuteMetrics.RetentionPolicy.Enabled).to.be.equal(false);
-            // Cors
-            expect(serviceProps.Cors).to.not.be.equal(null);
-            // Service Version String
-            expect(serviceProps.DefaultServiceVersion).to.be.equal("2013-08-15");
-            done();
-        });
+  // https://docs.microsoft.com/en-us/rest/api/storageservices/get-blob-service-properties
+  // at the time of writing this test Azurite API Version is : "2016-05-31";
+  describe("GET Blob Service Properties", () => {
+    it("should retrieve a valid set of blobService properties", (done) => {
+      expect(serviceProps).to.not.be.equal(undefined);
+      // Logging
+      expect(serviceProps.Logging.Version).to.be.equal("1.0");
+      expect(serviceProps.Logging.Delete).to.be.equal(true);
+      expect(serviceProps.Logging.Read).to.be.equal(true);
+      expect(serviceProps.Logging.Write).to.be.equal(true);
+      expect(serviceProps.Logging.RetentionPolicy.Enabled).to.be.equal(false);
+      // Hour Metrics
+      expect(serviceProps.HourMetrics.Version).to.be.equal("1.0");
+      expect(serviceProps.HourMetrics.Enabled).to.be.equal(false);
+      expect(serviceProps.HourMetrics.RetentionPolicy.Enabled).to.be.equal(
+        false
+      );
+      // // Minute Metrics
+      expect(serviceProps.MinuteMetrics.Version).to.be.equal("1.0");
+      expect(serviceProps.MinuteMetrics.Enabled).to.be.equal(false);
+      expect(serviceProps.MinuteMetrics.RetentionPolicy.Enabled).to.be.equal(
+        false
+      );
+      // Cors
+      expect(serviceProps.Cors).to.not.be.equal(null);
+      // Service Version String
+      expect(serviceProps.DefaultServiceVersion).to.be.equal("2013-08-15");
+      done();
     });
+  });
 
-    after(() => {
-        return azurite.close();
-      });
-
+  afterAll(() => {
+    return azurite.close();
+  });
 });
