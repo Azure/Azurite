48e933afaab0cbc54fc4a715fe4986a2b970bda7
Xiaoning Liu <xiaonli@microsoft.com>
2019-05-05 14:46:52 +0800

    Set containerACL expiry time from required to optional

--- src/blob/generated/artifacts/models.ts
+++ src/blob/generated/artifacts/models.ts
@@ -32,7 +32,7 @@ export interface AccessPolicy {
    * **NOTE: This entity will be treated as a string instead of a Date because the API can
    * potentially deal with a higher precision value than what is supported by JavaScript.**
    */
-  expiry: string;
+  expiry?: string;
   /**
    * the permissions for the acl policy
    */
