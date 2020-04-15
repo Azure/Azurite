0429c168038d08f276535afe5dbbddf5d6ab5ffb
XiaoningLiu <xiaoning.liu.leon@gmail.com>
2020-04-15 18:31:33 +0800

    Update version number to v3.7.0

--- src/blob/utils/constants.ts
+++ src/blob/utils/constants.ts
@@ -1,7 +1,7 @@
 import { StoreDestinationArray } from "../../common/persistence/IExtentStore";
 import * as Models from "../generated/artifacts/models";
 
-export const VERSION = "3.6.0";
+export const VERSION = "3.7.0";
 export const BLOB_API_VERSION = "2019-07-07";
 export const DEFAULT_BLOB_SERVER_HOST_NAME = "127.0.0.1"; // Change to 0.0.0.0 when needs external access
 export const DEFAULT_LIST_BLOBS_MAX_RESULTS = 5000;
