959a8c6b286f960fe5cb7b56483f165921476dbe
XiaoningLiu <xiaoning.liu.leon@gmail.com>
2020-07-26 21:54:31 +0800

    Bring queue dispath URI validate to strict (TODO: inconsistency between table/blob/queue, rewrite URI match logic)

--- src/queue/generated/utils/utils.ts
+++ src/queue/generated/utils/utils.ts
@@ -3,7 +3,7 @@ import URITemplate from "uri-templates";
 export function isURITemplateMatch(url: string, template: string): boolean {
   const uriTemplate = URITemplate(template);
   // TODO: Fixing $ parsing issue such as $logs container cannot work in strict mode issue
-  const result = (uriTemplate.fromUri as any)(url, { strict: false });
+  const result = (uriTemplate.fromUri as any)(url, { strict: true });
   if (result === undefined) {
     return false;
   }
