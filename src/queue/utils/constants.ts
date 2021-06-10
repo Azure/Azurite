6899ba38ca1df8abc8dd62b5c4584c5383e960f7
Wei Wei <weiwei@microsoft.com>
2021-06-10 16:43:11 +0800

    Upgrade version to 3.13.1

--- src/queue/utils/constants.ts
+++ src/queue/utils/constants.ts
@@ -1,6 +1,6 @@
 import { StoreDestinationArray } from "../../common/persistence/IExtentStore";
 
-export const VERSION = "3.13.0";
+export const VERSION = "3.13.1";
 export const QUEUE_API_VERSION = "2020-08-04";
 export const DEFAULT_QUEUE_SERVER_HOST_NAME = "127.0.0.1"; // Change to 0.0.0.0 when needs external access
 export const DEFAULT_QUEUE_LISTENING_PORT = 10001;
