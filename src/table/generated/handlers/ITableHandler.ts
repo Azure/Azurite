57ab789cc81f69dd3f3626acfc163ae618ae8936
Edwin Huber <edwin.huber@microsoft.com>
2022-02-01 10:07:18 +0100

    also reverted runyao's changes to ITableHandler

--- src/table/generated/handlers/ITableHandler.ts
+++ src/table/generated/handlers/ITableHandler.ts
@@ -17,13 +17,13 @@ export default interface ITableHandler {
   create(tableProperties: Models.TableProperties, options: Models.TableCreateOptionalParams, context: Context): Promise<Models.TableCreateResponse>;
   batch(body: NodeJS.ReadableStream, multipartContentType: string, contentLength: number, options: Models.TableBatchOptionalParams, context: Context): Promise<Models.TableBatchResponse>;
   delete(table: string, options: Models.TableDeleteMethodOptionalParams, context: Context): Promise<Models.TableDeleteResponse>;
-  queryEntities(table: string, options: Models.TableQueryEntitiesOptionalParams, context: Context, batchID?: string): Promise<Models.TableQueryEntitiesResponse>;
-  queryEntitiesWithPartitionAndRowKey(table: string, partitionKey: string, rowKey: string, options: Models.TableQueryEntitiesWithPartitionAndRowKeyOptionalParams, context: Context, batchID?: string): Promise<Models.TableQueryEntitiesWithPartitionAndRowKeyResponse>;
-  updateEntity(table: string, partitionKey: string, rowKey: string, options: Models.TableUpdateEntityOptionalParams, context: Context, batchID?: string): Promise<Models.TableUpdateEntityResponse>;
-  mergeEntity(table: string, partitionKey: string, rowKey: string, options: Models.TableMergeEntityOptionalParams, context: Context, batchID?: string): Promise<Models.TableMergeEntityResponse>;
-  deleteEntity(table: string, partitionKey: string, rowKey: string, ifMatch: string, options: Models.TableDeleteEntityOptionalParams, context: Context, batchID?: string): Promise<Models.TableDeleteEntityResponse>;
+  queryEntities(table: string, options: Models.TableQueryEntitiesOptionalParams, context: Context): Promise<Models.TableQueryEntitiesResponse>;
+  queryEntitiesWithPartitionAndRowKey(table: string, partitionKey: string, rowKey: string, options: Models.TableQueryEntitiesWithPartitionAndRowKeyOptionalParams, context: Context): Promise<Models.TableQueryEntitiesWithPartitionAndRowKeyResponse>;
+  updateEntity(table: string, partitionKey: string, rowKey: string, options: Models.TableUpdateEntityOptionalParams, context: Context): Promise<Models.TableUpdateEntityResponse>;
+  mergeEntity(table: string, partitionKey: string, rowKey: string, options: Models.TableMergeEntityOptionalParams, context: Context): Promise<Models.TableMergeEntityResponse>;
+  deleteEntity(table: string, partitionKey: string, rowKey: string, ifMatch: string, options: Models.TableDeleteEntityOptionalParams, context: Context): Promise<Models.TableDeleteEntityResponse>;
   mergeEntityWithMerge(table: string, partitionKey: string, rowKey: string, options: Models.TableMergeEntityWithMergeOptionalParams, context: Context): Promise<Models.TableMergeEntityWithMergeResponse>;
-  insertEntity(table: string, options: Models.TableInsertEntityOptionalParams, context: Context, batchID?: string): Promise<Models.TableInsertEntityResponse>;
+  insertEntity(table: string, options: Models.TableInsertEntityOptionalParams, context: Context): Promise<Models.TableInsertEntityResponse>;
   getAccessPolicy(table: string, options: Models.TableGetAccessPolicyOptionalParams, context: Context): Promise<Models.TableGetAccessPolicyResponse>;
   setAccessPolicy(table: string, options: Models.TableSetAccessPolicyOptionalParams, context: Context): Promise<Models.TableSetAccessPolicyResponse>;
 }
