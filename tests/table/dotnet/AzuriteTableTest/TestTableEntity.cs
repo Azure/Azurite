9a9f4f590ded4a6defac1323c157f48ef63c40ef
Edwin Huber <edwin.huber@microsoft.com>
2022-08-26 20:55:06 +0200

    adding dotnete samples to test azure data tables and cosmos SDKs

---
+++ tests/table/dotnet/AzuriteTableTest/TestTableEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzuriteTableTest
+{
+    internal class TestTableEntity : TableEntity
+    {
+        public double Value { get; set; }
+
+        public TestTableEntity()
+        {
+        }
+
+        public TestTableEntity(string partitionKey, string rowKey, double value)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+            Value = value;
+        }
+
+        public TestTableEntity(double value)
+        {
+            Value = value;
+        }
+    }
+
+}
