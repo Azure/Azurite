cb98a686121073c256baefa4d49cecb066549b1d
Emma Zhu <emmazhu@microsoft.com>
2023-08-16 14:53:17 +0800

    Bump service version to 2023-08-03

--- src/queue/utils/constants.ts
+++ src/queue/utils/constants.ts
@@ -1,7 +1,7 @@
 import { StoreDestinationArray } from "../../common/persistence/IExtentStore";
 
-export const VERSION = "3.25.1";
-export const QUEUE_API_VERSION = "2023-01-03";
+export const VERSION = "3.26.0";
+export const QUEUE_API_VERSION = "2023-08-03";
 export const DEFAULT_QUEUE_SERVER_HOST_NAME = "127.0.0.1"; // Change to 0.0.0.0 when needs external access
 export const DEFAULT_QUEUE_LISTENING_PORT = 10001;
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
@@ -90,6 +90,7 @@ export const DEFAULT_QUEUE_PERSISTENCE_ARRAY: StoreDestinationArray = [
 ];
 
 export const ValidAPIVersions = [
+  "2023-08-03",
   "2023-01-03",
   "2022-11-02",
   "2021-12-02",
