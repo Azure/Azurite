049568ed07c2e839035fe1736de2ab0f2b746067
Edwin Huber <edwinhub@microsoft.com>
2023-09-13 14:28:06 +0200

    update to issue 793 test

--- tests/table/dotnet/AzuriteTableTest/DataTableTests.cs
+++ tests/table/dotnet/AzuriteTableTest/DataTableTests.cs
@@ -5,14 +5,12 @@ using NUnit.Framework;
 using System.Threading.Tasks;
 using Azure.Data.Tables;
 using System.Collections;
-
 namespace AzuriteTableTest
 {
-  [TestFixture]
-  public class DataTablesTests
-  {
-
-    [SetUp]
+    [TestFixture]
+    public class DataTablesTests
+    {
+        [SetUp]
     public void Setup()
     {
     }
@@ -38,40 +36,37 @@ namespace AzuriteTableTest
         await table.SubmitTransactionAsync(actions);
       }
       catch (TableTransactionFailedException ex)
-      {
-        Assert.Fail(ex.ToString());
-      }
-
-      Assert.Pass();
-
-    }
-
-    [Test]
-    public async Task TestForIssue791()
-    {
-      var client = new TableServiceClient("UseDevelopmentStorage=true");
-      var table = client.GetTableClient("test");
-      await table.CreateIfNotExistsAsync();
-
-      var pk = Guid.NewGuid().ToString();
-      await table.AddEntityAsync(new TableEntity(pk, "a"));
-
-      var actions = new[]
-      {
+            {
+                // When replacing an entity with an etag condition, the Azurite table emulator should not
+                // accept the entity replace if the etag is wrong.
+                // (i.e.does not match the current entity state).
+                // Azure and the legacy emulator return HTTP 412 Precondition failed.
+                Assert.AreEqual(ex.Status, 412);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public async Task TestForIssue791()
+        {
+            var client = new TableServiceClient("UseDevelopmentStorage=true");
+            var table = client.GetTableClient("test");
+            await table.CreateIfNotExistsAsync();
+            var pk = Guid.NewGuid().ToString();
+            await table.AddEntityAsync(new TableEntity(pk, "a"));
+            var actions = new[]
+            {
                 new TableTransactionAction(TableTransactionActionType.Add, new TableEntity(pk, "a")),
             };
-
-      try
-      {
-        await table.SubmitTransactionAsync(actions);
-      }
-      catch (TableTransactionFailedException ex)
-      {
-        Assert.Fail(ex.ToString());
-      }
-
-      Assert.Pass();
-    }
+            try
+            {
+                await table.SubmitTransactionAsync(actions);
+            }
+            catch (TableTransactionFailedException ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+            Assert.Pass();
+        }
 
     [Test]
     public async Task TestForIssue1286()
