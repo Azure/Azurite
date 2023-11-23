a868978582394821eb38cb509ddce3386e25d61c
Emma Zhu <emmazhu@microsoft.com>
2023-11-23 16:21:54 +0800

    Bump Azurite version for release

--- src/queue/utils/constants.ts
+++ src/queue/utils/constants.ts
@@ -1,6 +1,6 @@
 import { StoreDestinationArray } from "../../common/persistence/IExtentStore";
 
-export const VERSION = "3.27.0";
+export const VERSION = "3.28.0";
 export const QUEUE_API_VERSION = "2023-11-03";
 export const DEFAULT_QUEUE_SERVER_HOST_NAME = "127.0.0.1"; // Change to 0.0.0.0 when needs external access
 export const DEFAULT_QUEUE_LISTENING_PORT = 10001;
