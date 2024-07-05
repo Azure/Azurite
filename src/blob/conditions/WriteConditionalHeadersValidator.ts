3aa318f391462017a7d12a7b92a8d27e0c024f2f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-07-05 02:42:04 -0400

    spelling: return
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/blob/conditions/WriteConditionalHeadersValidator.ts
+++ src/blob/conditions/WriteConditionalHeadersValidator.ts
@@ -124,7 +124,7 @@ export default class WriteConditionalHeadersValidator
         if (conditionalHeaders.ifNoneMatch[0] === "*") {
           // According to restful doc, specify the wildcard character (*) to perform the operation
           // only if the resource does not exist, and fail the operation if it does exist.
-          // However, Azure Storage Set Blob Properties Operation for an existing blob doesn't reuturn 412 with *
+          // However, Azure Storage Set Blob Properties Operation for an existing blob doesn't return 412 with *
           // TODO: Check accurate behavior for different write operations
           // Put Blob, Commit Block List has special logic for ifNoneMatch equals *, will return 409 conflict for existing blob, will handled in createBlob metadata store.
           // throw StorageErrorFactory.getConditionNotMet(context.contextId!);
