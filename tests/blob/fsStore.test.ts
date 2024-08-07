Azure/Azurite

commit a527e2a4056cb4f9dac21f77b74faff09a6bf772

Fixed an issue where premature client disconnections led to all following requests failing with a 500 error. (#2433)
The underlying file, where the data is saved was closed if any error occurred while reading from the client connection. Subsequently, the file was attempted to be used again, but it is closed.

src/common/persistence/FSExtentStore.ts
@@ -3,6 +3,7 @@ import {
   createReadStream,
   createWriteStream,
   fdatasync,
+  ftruncate,
   mkdir,
   open,
   stat,
@@ -34,6 +35,7 @@ import OperationQueue from "./OperationQueue";
 const statAsync = promisify(stat);
 const mkdirAsync = promisify(mkdir);
 const unlinkAsync = promisify(unlink);
+const ftruncateAsync = promisify(ftruncate);
 
 // The max size of an extent.
 const MAX_EXTENT_SIZE = DEFAULT_MAX_EXTENT_SIZE;
@@ -293,6 +295,17 @@ export default class FSExtentStore implements IExtentStore {
               count
             };
           } catch (err) {
+            // Reset cursor position to the current offset.
+            try {
+              await ftruncateAsync(fd, appendExtent.offset);
+            } catch (truncate_err) {
+              this.logger.error(
+                `FSExtentStore:appendExtent() Truncate fd:${fd} len: ${appendExtent.offset} error:${JSON.stringify(
+                  truncate_err
+                )}.`,
+                contextId
+              );
+            }
             appendExtent.appendStatus = AppendStatusCode.Idle;
             throw err;
           }
@@ -541,10 +554,11 @@ export default class FSExtentStore implements IExtentStore {
           this.logger.debug(
             `FSExtentStore:streamPipe() Readable stream triggers error event, error:${JSON.stringify(
               err
-            )}, after ${count} bytes piped. Invoke write stream destroy() method.`,
+            )}, after ${count} bytes piped. Reject streamPipe().`,
             contextId
           );
-          ws.destroy(err);
+          
+          reject(err);
         });
 
       ws.on("drain", () => {

tests/blob/fsStore.test.ts
@@ -1,0 +1,38 @@
+import assert from "assert";
+import { Readable } from "stream";
+import FSExtentStore from "../../src/common/persistence/FSExtentStore";
+import IExtentMetadataStore from "../../src/common/persistence/IExtentMetadataStore";
+import { DEFAULT_BLOB_PERSISTENCE_ARRAY } from "../../src/blob/utils/constants";
+import logger from "../../src/common/Logger";
+
+import { mock } from "ts-mockito";
+
+describe("FSExtentStore", () => {
+
+  const metadataStore: IExtentMetadataStore = mock<IExtentMetadataStore>();
+  metadataStore.getExtentLocationId = () => Promise.resolve("Default");
+
+  it("should handle input stream error gracefully during appendExtent @loki", async () => {
+    const store = new FSExtentStore(metadataStore, DEFAULT_BLOB_PERSISTENCE_ARRAY, logger);
+    await store.init();
+
+    // A null value within the Readable.from array causes the stream to emit an error.
+    const stream1 = Readable.from(["deadbeef", null], { objectMode: false });
+    await assert.rejects(store.appendExtent(stream1));
+
+    // Write a valid stream to the store.
+    const stream2 = Readable.from("Test", { objectMode: false });
+    const extent = await store.appendExtent(stream2);
+    assert.strictEqual(extent.offset, 0);
+    assert.strictEqual(extent.count, 4);
+
+    // Check that the extent is readable.
+    let readable = await store.readExtent(extent);
+    const chunks: Buffer[] = [];
+    for await (const chunk of readable) {
+      chunks.push(chunk as Buffer);
+    }
+    const data = Buffer.concat(chunks);
+    assert.strictEqual(data.toString(), "Test");
+  });
+});

commit 128ea4142fea529c9f113cde43537b3845c69ac0

Fixed failing test on Windows caused by #2433 (#2446)
* Fixed failing test on Windows.

On Windows to truncate the file, which is opened in append mode, the file first needs to be closed. Expanded the test case to include another valid extent, this time prior to failed update.

* Remove redundant changelog entry.

src/common/persistence/FSExtentStore.ts
@@ -3,7 +3,7 @@ import {
   createReadStream,
   createWriteStream,
   fdatasync,
-  ftruncate,
+  truncate,
   mkdir,
   open,
   stat,
@@ -35,7 +35,7 @@ import OperationQueue from "./OperationQueue";
 const statAsync = promisify(stat);
 const mkdirAsync = promisify(mkdir);
 const unlinkAsync = promisify(unlink);
-const ftruncateAsync = promisify(ftruncate);
+const truncateAsync = promisify(truncate);
 
 // The max size of an extent.
 const MAX_EXTENT_SIZE = DEFAULT_MAX_EXTENT_SIZE;
@@ -295,18 +295,22 @@ export default class FSExtentStore implements IExtentStore {
               count
             };
           } catch (err) {
-            // Reset cursor position to the current offset.
+            // Reset cursor position to the current offset. On Windows, truncating a file open in append mode doesn't
+            // work, so we need to close the file descriptor first.
             try {
-              await ftruncateAsync(fd, appendExtent.offset);
+              appendExtent.fd = undefined;
+              await closeAsync(fd);
+              await truncateAsync(path, appendExtent.offset);
+              // Indicate that the extent is ready for the next append operation.
+              appendExtent.appendStatus = AppendStatusCode.Idle;
             } catch (truncate_err) {
               this.logger.error(
-                `FSExtentStore:appendExtent() Truncate fd:${fd} len: ${appendExtent.offset} error:${JSON.stringify(
+                `FSExtentStore:appendExtent() Truncate path:${path} len: ${appendExtent.offset} error:${JSON.stringify(
                   truncate_err
                 )}.`,
                 contextId
               );
             }
-            appendExtent.appendStatus = AppendStatusCode.Idle;
             throw err;
           }
         })()

tests/blob/fsStore.test.ts
@@ -12,27 +12,39 @@ describe("FSExtentStore", () => {
   const metadataStore: IExtentMetadataStore = mock<IExtentMetadataStore>();
   metadataStore.getExtentLocationId = () => Promise.resolve("Default");
 
-  it("should handle input stream error gracefully during appendExtent @loki", async () => {
-    const store = new FSExtentStore(metadataStore, DEFAULT_BLOB_PERSISTENCE_ARRAY, logger);
-    await store.init();
-
-    // A null value within the Readable.from array causes the stream to emit an error.
-    const stream1 = Readable.from(["deadbeef", null], { objectMode: false });
-    await assert.rejects(store.appendExtent(stream1));
-
-    // Write a valid stream to the store.
-    const stream2 = Readable.from("Test", { objectMode: false });
-    const extent = await store.appendExtent(stream2);
-    assert.strictEqual(extent.offset, 0);
-    assert.strictEqual(extent.count, 4);
-
-    // Check that the extent is readable.
-    let readable = await store.readExtent(extent);
+  async function readIntoString(readable: NodeJS.ReadableStream): Promise<string> {
     const chunks: Buffer[] = [];
     for await (const chunk of readable) {
       chunks.push(chunk as Buffer);
     }
-    const data = Buffer.concat(chunks);
-    assert.strictEqual(data.toString(), "Test");
+    const buffer = Buffer.concat(chunks);
+    return buffer.toString();
+  }
+
+  it("should handle input stream error gracefully during appendExtent @loki", async () => {
+    const store = new FSExtentStore(metadataStore, DEFAULT_BLOB_PERSISTENCE_ARRAY, logger);
+    await store.init();
+
+    // Write a valid stream to the store.
+    const stream1 = Readable.from("First", { objectMode: false });
+    const extent1 = await store.appendExtent(stream1);
+    assert.strictEqual(extent1.offset, 0);
+    assert.strictEqual(extent1.count, 5);
+
+    // A null value within the Readable.from array causes the stream to emit an error.
+    const stream2 = Readable.from(["deadbeef", null], { objectMode: false });
+    await assert.rejects(store.appendExtent(stream2));
+
+    // Write another valid stream to the store.
+    const stream3 = Readable.from("Test", { objectMode: false });
+    const extent3 = await store.appendExtent(stream3);
+    assert.strictEqual(extent3.offset, 5);
+    assert.strictEqual(extent3.count, 4);
+
+    // Check that the extents is readable.
+    let readable1 = await store.readExtent(extent1);
+    assert.strictEqual(await readIntoString(readable1), "First");
+    let readable3 = await store.readExtent(extent3);
+    assert.strictEqual(await readIntoString(readable3), "Test");
   });
 });