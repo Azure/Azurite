43f15e994e9dc53bb34b514b77b8d87bc2a959ac
HerrDerb <truuba@gmail.com>
2026-03-18 11:11:58 +0100

    Add missing tests

---
+++ tests/queue/queueEnvironment.test.ts
@@ -0,0 +1,47 @@
+import * as assert from "assert";
+
+import QueueEnvironment from "../../src/queue/QueueEnvironment";
+
+describe("QueueEnvironment", () => {
+  const originalArgv = process.argv;
+
+  beforeEach(() => {
+    process.argv = ["node", "azurite-queue"];
+    delete process.env.AZURITE_SKIP_API_VERSION_CHECK;
+  });
+
+  afterEach(() => {
+    process.argv = originalArgv;
+    delete process.env.AZURITE_SKIP_API_VERSION_CHECK;
+  });
+
+  it("defaults skipApiVersionCheck to false @loki", () => {
+    const env = new QueueEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), false);
+  });
+
+  it("returns true when env var AZURITE_SKIP_API_VERSION_CHECK is true @loki", () => {
+    process.env.AZURITE_SKIP_API_VERSION_CHECK = "true";
+
+    const env = new QueueEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), true);
+  });
+
+  it("returns false when env var AZURITE_SKIP_API_VERSION_CHECK is false @loki", () => {
+    process.env.AZURITE_SKIP_API_VERSION_CHECK = "false";
+
+    const env = new QueueEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), false);
+  });
+
+  it("returns true when skipApiVersionCheck flag is set @loki", () => {
+    process.argv.push("--skipApiVersionCheck");
+
+    const env = new QueueEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), true);
+  });
+});
---
+++ tests/table/tableEnvironment.test.ts
@@ -0,0 +1,47 @@
+import * as assert from "assert";
+
+import TableEnvironment from "../../src/table/TableEnvironment";
+
+describe("TableEnvironment", () => {
+  const originalArgv = process.argv;
+
+  beforeEach(() => {
+    process.argv = ["node", "azurite-table"];
+    delete process.env.AZURITE_SKIP_API_VERSION_CHECK;
+  });
+
+  afterEach(() => {
+    process.argv = originalArgv;
+    delete process.env.AZURITE_SKIP_API_VERSION_CHECK;
+  });
+
+  it("defaults skipApiVersionCheck to false @loki", () => {
+    const env = new TableEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), false);
+  });
+
+  it("returns true when env var AZURITE_SKIP_API_VERSION_CHECK is true @loki", () => {
+    process.env.AZURITE_SKIP_API_VERSION_CHECK = "true";
+
+    const env = new TableEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), true);
+  });
+
+  it("returns false when env var AZURITE_SKIP_API_VERSION_CHECK is false @loki", () => {
+    process.env.AZURITE_SKIP_API_VERSION_CHECK = "false";
+
+    const env = new TableEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), false);
+  });
+
+  it("returns true when skipApiVersionCheck flag is set @loki", () => {
+    process.argv.push("--skipApiVersionCheck");
+
+    const env = new TableEnvironment();
+
+    assert.strictEqual(env.skipApiVersionCheck(), true);
+  });
+});
