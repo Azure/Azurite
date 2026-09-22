cfff32ed9ea248ff42a7843d218a1ff9b08f1da7
Akanksha Jain <jainakanksha@microsoft.com>
2026-09-22 18:35:13 +0530

    Fix blob lease error precedence (#2798)

---
+++ src/blob/conditions/BlobCreateConditionsAndLeaseSync.ts
@@ -0,0 +1,29 @@
+import StorageErrorFactory from "../errors/StorageErrorFactory";
+import * as Models from "../generated/artifacts/models";
+import Context from "../generated/Context";
+import BlobLeaseAdapter from "../lease/BlobLeaseAdapter";
+import BlobWriteLeaseSyncer from "../lease/BlobWriteLeaseSyncer";
+import BlobWriteLeaseValidator from "../lease/BlobWriteLeaseValidator";
+import LeaseFactory from "../lease/LeaseFactory";
+import { BlobModel } from "../persistence/IBlobMetadataStore";
+
+export default function validateBlobCreateConditionsAndSyncLease(
+  context: Context,
+  existingBlob: BlobModel,
+  destinationBlob: BlobModel,
+  leaseAccessConditions?: Models.LeaseAccessConditions,
+  modifiedAccessConditions?: Models.ModifiedAccessConditions
+): void {
+  const leaseState = LeaseFactory.createLeaseState(
+    new BlobLeaseAdapter(existingBlob),
+    context
+  );
+  leaseState.validate(new BlobWriteLeaseValidator(leaseAccessConditions));
+
+  // Azure validates an active lease before reporting that the blob exists.
+  if (modifiedAccessConditions?.ifNoneMatch === "*") {
+    throw StorageErrorFactory.getBlobAlreadyExists(context.contextId);
+  }
+
+  leaseState.sync(new BlobWriteLeaseSyncer(destinationBlob));
+}
